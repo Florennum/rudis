c24dbeaf5a410e023302ba858eb1b9fe273b0518
Florennum <spasthebg@gmail.com>
2023-11-01 20:46:46 +0200

    haha yes

---
+++ common/setge/setge.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/user"
+	"path/filepath"
+
+	"github.com/Florennum/rudis/common/fetchtag"
+	tomlv2 "github.com/pelletier/go-toml/v2"
+)
+
+func main() {
+	// Get the current user's home directory
+	currentUser, err := user.Current()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tag, err := fetchtag.FetchTag()
+	if err != nil {
+		return
+	}
+
+	// Define the path to the TOML configuration file
+	configFile := filepath.Join(currentUser.HomeDir, ".config", "vinegar", "config.toml")
+
+	// Read the contents of the TOML configuration file
+	fileContent, err := os.ReadFile(configFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var config map[string]interface{}
+
+	// Unmarshal the TOML content into a Go data structure
+	err = tomlv2.Unmarshal(fileContent, &config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Update the 'wineroot' value in the TOML data structure
+	extractedDir := filepath.Join(currentUser.HomeDir, ".local", "share", "rudis", "winege-ext")
+	newValue := filepath.Join(extractedDir, fmt.Sprintf("lutris-%s-x86_64", tag))
+	config["wineroot"] = newValue
+
+	// Marshal the updated data back to TOML format
+	updatedTOML, err := tomlv2.Marshal(config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Write the updated configuration back to the file
+	err = os.WriteFile(configFile, updatedTOML, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Updated 'wineroot' in the configuration file:", configFile)
+}
--- go.mod
+++ go.mod
@@ -3,12 +3,8 @@ module github.com/Florennum/rudis
 go 1.21.3
 
 require github.com/google/go-github v17.0.0+incompatible
-require github.com/faith/color v1.15.0
 
 require (
-	github.com/fatih/color v1.15.0 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
-	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.17 // indirect
-	golang.org/x/sys v0.6.0 // indirect
+	github.com/pelletier/go-toml/v2 v2.1.0
 )
